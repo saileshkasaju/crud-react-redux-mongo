9a151a8c2e7626aae1986f99453cbda58056a302
Sailesh Kasaju <leosailesh@gmail.com>
2017-01-18 18:00:34 +0545

    first route to GamesPage component

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import { Link, Match } from 'react-router';
 import logo from './logo.svg';
+import GamesPage from './GamesPage';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    <Link to="games">Games</Link>
+                </p>
+                <Match pattern="/games" component={GamesPage}/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ crud/src/GamesPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class GamesPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Games List</h1>
+            </div>
+        );
+    }
+}
+
+export default GamesPage;
