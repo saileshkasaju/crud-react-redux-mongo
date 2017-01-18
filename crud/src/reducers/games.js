d84d687e4dca2eab3ddc6895c0c86d9262fc089c
Sailesh Kasaju <leosailesh@gmail.com>
2017-01-18 17:51:13 +0545

    added redux-devtools-extension, wrapped with Provider and added games reducer

---
+++ crud/src/reducers/games.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sailesh on 1/18/17.
+ */
+export default function games(state = [], action = {}) {
+    switch(action.type) {
+        default: return state;
+    }
+}
---
+++ crud/src/rootReducer.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sailesh on 1/18/17.
+ */
+import {combineReducers } from 'redux';
+
+import games from './reducers/games';
+
+export default combineReducers({
+    games
+})
