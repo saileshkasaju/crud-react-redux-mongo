8f89b7d0f0c925899c400d826c10bffa7ed35ad1
saileshkasaju <leosailesh@gmail.com>
2017-11-02 23:22:02 +0545

    enable cors in api

--- crud/backend/server.js
+++ crud/backend/server.js
@@ -1,87 +1,98 @@
-import express from 'express';
-import mongodb from 'mongodb';
-import bodyParser from 'body-parser';
+import express from "express";
+import cors from "cors";
+import mongodb from "mongodb";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 // const dbUrl = 'mongodb://localhost/crudwithredux';
-const dbUrl = 'mongodb://react:reactpass@ds137149.mlab.com:37149/shared';
+const dbUrl = "mongodb://react:reactpass@ds137149.mlab.com:37149/shared";
 
 function validate(data) {
   let errors = {};
-  if (data.title === '') errors.title = "Can't be empty";
-  if (data.cover === '') errors.cover = "Can't be empty";
+  if (data.title === "") errors.title = "Can't be empty";
+  if (data.cover === "") errors.cover = "Can't be empty";
   const isValid = Object.keys(errors).length === 0;
   return { errors, isValid };
 }
 
 mongodb.MongoClient.connect(dbUrl, function(err, db) {
-  
-  app.get('/api/games', (req, res) => {
-    db.collection('games').find({}).toArray((err, games) => {
-      res.json({ games });
-    });
+  app.get("/api/games", (req, res) => {
+    db
+      .collection("games")
+      .find({})
+      .toArray((err, games) => {
+        res.json({ games });
+      });
   });
-  
-  app.post('/api/games', (req, res) => {
+
+  app.post("/api/games", (req, res) => {
     const { errors, isValid } = validate(req.body);
     if (isValid) {
       const { title, cover } = req.body;
-      db.collection('games').insert({ title, cover }, (err, result) => {
+      db.collection("games").insert({ title, cover }, (err, result) => {
         if (err) {
-          res.status(500).json({ errors: { global: "Something went wrong" }});
+          res.status(500).json({ errors: { global: "Something went wrong" } });
         } else {
-          res.json({ game: result.ops[0] })
+          res.json({ game: result.ops[0] });
         }
       });
     } else {
-      res.status(400).json({ errors })
+      res.status(400).json({ errors });
     }
   });
-  
-  app.put('/api/games/:_id', (req, res) => {
+
+  app.put("/api/games/:_id", (req, res) => {
     const { errors, isValid } = validate(req.body);
-    
+
     if (isValid) {
       const { title, cover } = req.body;
-      db.collection('games').findOneAndUpdate(
-        { _id: new mongodb.ObjectID(req.params._id) },
-        { $set: { title, cover} },
-        { returnOriginal: false },
-        (err, result) => {
-          if (err) { res.status(500).json({ errors: { global: err }}); return; }
-          res.json({ game: result.value });
-        }
-      )
+      db
+        .collection("games")
+        .findOneAndUpdate(
+          { _id: new mongodb.ObjectID(req.params._id) },
+          { $set: { title, cover } },
+          { returnOriginal: false },
+          (err, result) => {
+            if (err) {
+              res.status(500).json({ errors: { global: err } });
+              return;
+            }
+            res.json({ game: result.value });
+          }
+        );
     }
   });
-  
-  app.get('/api/games/:_id', (req, res) => {
-    db.collection('games').findOne({ _id: new mongodb.ObjectID(req.params._id) }, (err, game) => {
-      res.json({ game })
-    })
+
+  app.get("/api/games/:_id", (req, res) => {
+    db
+      .collection("games")
+      .findOne({ _id: new mongodb.ObjectID(req.params._id) }, (err, game) => {
+        res.json({ game });
+      });
   });
-  
-  app.delete('/api/games/:_id', (req, res) => {
-    db.collection('games').deleteOne({ _id: new mongodb.ObjectID(req.params._id) },
-      (err, r) => {
-        if (err) { res.status(500).json({ errors: { global: err }});
+
+  app.delete("/api/games/:_id", (req, res) => {
+    db
+      .collection("games")
+      .deleteOne({ _id: new mongodb.ObjectID(req.params._id) }, (err, r) => {
+        if (err) {
+          res.status(500).json({ errors: { global: err } });
           return;
         }
         res.json({});
-      })
+      });
   });
-  
+
   app.use((req, res) => {
     res.status(404).json({
       errors: {
-        global: "Still working on it. Please try again later when we implement it"
+        global:
+          "Still working on it. Please try again later when we implement it"
       }
-    })
+    });
   });
-  
-  
-  app.listen(8080, () => console.log('Server is running on localhost:8080'));
-  
-});
 
+  app.listen(8080, () => console.log("Server is running on localhost:8080"));
+});
