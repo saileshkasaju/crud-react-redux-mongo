f33389e56218dd6587492d277b3dc029778bd4cb
saileshkasaju <leosailesh@gmail.com>
2017-11-02 23:12:58 +0545

    update api

--- crud/src/actions.js
+++ crud/src/actions.js
@@ -6,7 +6,7 @@ export const ADD_GAME = "ADD_GAME";
 export const GAME_FETCHED = "GAME_FETCHED";
 export const GAME_UPDATED = "GAME_UPDATED";
 export const GAME_DELETED = "GAME_DELETED";
-const BASE_URL = "https://backend-odjwgfvmkh.now.sh/";
+const BASE_URL = "https://backend-odjwgfvmkh.now.sh";
 
 function handleResponse(response) {
   if (response.ok) {
