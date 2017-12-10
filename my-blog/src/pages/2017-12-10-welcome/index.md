a4b334c03c512ac848af771cdfb6c1131edafe00
saileshkasaju <leosailesh@gmail.com>
2017-12-10 14:33:32 +0545

    clean code to gatsby blog

---
+++ my-blog/src/pages/2017-12-10-welcome/index.md
@@ -0,0 +1,12 @@
+---
+path: "/welcome"
+date: "2017-12-10T11:56:59.165Z"
+title: "Welcome to my js blog"
+tags: ['first post', 'js', 'welcome']
+excerpt: "Welcome to my js blog post series!"
+---
+
+# ARRRROOOOOOOOOO
+
+This is my first blog post. It is a good day today. warm sun outside in this
+winter month...
---
+++ my-blog/src/pages/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const IndexPage = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark;
+  return (
+    <div>
+      {posts.map(({ node: post }, index) => {
+        const { frontmatter } = post;
+
+        return (
+          <div key={`post_index_${index}`}>
+            <h2>
+              <Link to={frontmatter.path}>{frontmatter.title}</Link>
+            </h2>
+            <p>{frontmatter.date}</p>
+            <p>{frontmatter.excerpt}</p>
+            <ul>
+              {post.frontmatter.tags.map(tag => {
+                return (
+                  <li key={`post_tags_${tag}`}>
+                    <Link to={`/tags/${tag}`}>{tag}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+            tags
+            excerpt
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default IndexPage;
