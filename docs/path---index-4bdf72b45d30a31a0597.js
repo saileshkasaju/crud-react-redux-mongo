bb0c151fa7296a8525895d53c56e7adcc228291c
saileshkasaju <leosailesh@gmail.com>
2017-12-10 22:48:40 +0545

    remove google webmaster verification file

--- docs/path---index-4bdf72b45d30a31a0597.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x81b8806e4260],{310:function(e,o){e.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{id:"/home/sailesh/workspace/crud-react-redux-mongo/my-blog/src/pages/2017-12-10-welcome/index.md absPath of file >>> MarkdownRemark",frontmatter:{title:"Hello JS World",date:"December 10, 2017",path:"/hello-world",tags:["first post","js","hello world","welcome"],excerpt:"JS hello world blog post"}}}]}},pathContext:{}}}});
-//# sourceMappingURL=path---index-4bdf72b45d30a31a0597.js.map
